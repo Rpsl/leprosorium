a6af246169dd39b04d12fa7db32e5c7f414de25c
Rpsl <gmail@rpsl.info>
2014-05-17 21:41:48 +0400

    fixes in youtube script

--- common/scripts/youtube_link_convert.js
+++ common/scripts/youtube_link_convert.js
@@ -13,20 +13,21 @@ function main() {
 
         var id = (a.href.search(/&/) != -1) ? a.href.slice(a.href.search(/=/) + 1, a.href.search(/&/)) : a.href.slice(a.href.search(/=/) + 1);
 
-//        var obj = document.createElement('object');
-//        obj.width = 640; obj.height = 385;
-//        obj.innerHTML = '<param value="http://www.youtube.com/v/' + id + '" name="movie"/><param name="allowFullScreen" value="true" /><param value="transparent" name="wmode"/><embed width="640" height="385" wmode="transparent" type="application/x-shockwave-flash" src="http://www.youtube.com/v/' + id + '"/><a href="http://www.youtube.com/v/' + id + '" />';
-
         // <iframe src="http://www.youtube.com/embed/kJ1OdKdvg4g" height="480" width="640" allowfullscreen="" frameborder="0"></iframe>
 
         var obj = document.createElement('p');
         obj.style = "padding: 0; margin: 0";
-        obj.innerHTML = '<iframe src="http://www.youtube.com/embed/'+id+'" height="480" width="640" allowfullscreen="" frameborder="0"></iframe>';
+
+        var iframe = document.createElement('iframe');
+        iframe.src = "http://www.youtube.com/embed/" + id;
+        iframe.height = 480;
+        iframe.width = 640;
+        iframe.setAttribute('allowfullscreen', true);
+        iframe.setAttribute('frameborder', 0);
+
+        obj.appendChild(iframe);
 
         insertAfter( obj, a );
-//        a.parentNode.insertAfter(obj,a);
-//        a.parentNode.insertBefore(obj,a);
-//        a.parentNode.removeChild(a);
     }
 }
 
