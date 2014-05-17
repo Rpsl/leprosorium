fa12862e96f3dd34fdb390fc5d32deef5e66c3b0
Rpsl <gmail@rpsl.info>
2014-05-18 03:38:23 +0400

    you tube links converted after load next page

--- common/scripts/youtube_link_convert.js
+++ common/scripts/youtube_link_convert.js
@@ -6,11 +6,17 @@
 // @include		http://leprosorium.ru/*
 // ==/UserScript==
 
-function main() {
+function handle() {
     var anchors = document.getElementsByTagName('a');
     for(var i = 0, a; a = anchors[i]; ++i){
         if(a.href.search(/youtube\.com\/watch\?v=/) == -1 || a.parentNode.className == "Quote") continue;
 
+        if( a.classList.contains('converted') )
+        {
+            continue;
+        }
+
+        a.classList.add('converted');
         var id = (a.href.search(/&/) != -1) ? a.href.slice(a.href.search(/=/) + 1, a.href.search(/&/)) : a.href.slice(a.href.search(/=/) + 1);
 
         // <iframe src="http://www.youtube.com/embed/kJ1OdKdvg4g" height="480" width="640" allowfullscreen="" frameborder="0"></iframe>
@@ -30,6 +36,15 @@ function main() {
         insertAfter( obj, a );
     }
 }
+
+function main(){
+    // If DOM changed
+    document.addEventListener("DOMNodeInserted", handle, false);
+
+    handle();
+}
+
+
 
 function insertAfter(elem, refElem) {
     return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
